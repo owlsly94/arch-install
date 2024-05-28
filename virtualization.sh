970c1982f231616269c10ddff53db18809acd61d
owlsly94 <stefan.stanimirovic09@gmail.com>
2024-05-28 20:24:31 +0200

    adding virtualization

---
+++ virtualization.sh
@@ -0,0 +1,9 @@
+yay -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
+
+sudo systemctl enable libvirtd.service
+
+sudo systemctl start libvirtd.service
+
+sudo usermod -a -G libvirt $(whoami)
+
+sudo systemctl restart libvirtd.service
