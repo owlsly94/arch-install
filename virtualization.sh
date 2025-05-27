c48ac2d61ace095ae348830c1c07945ea12d108f
owlsly94 <stefan.stanimirovic09@gmail.com>
2025-05-27 11:05:51 +0200

    virtualization

--- virtualization.sh
+++ virtualization.sh
@@ -1,9 +1,15 @@
-paru -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
+paru -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat ebtables iptables libguestfs
+
+sudo nvim /etc/libvirt/libvirtd.conf #uncheck:
+# unix_sock_group = "libvirt"
+# unix_sock_rw_perms = "0770"
 
 sudo systemctl enable libvirtd.service
 
 sudo systemctl start libvirtd.service
 
 sudo usermod -a -G libvirt $(whoami)
+
+newgrp libvirt
 
 sudo systemctl restart libvirtd.service
